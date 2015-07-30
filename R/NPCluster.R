9ac300f361d8c3d2dfbe4cd99260748456143e37
Marc Suchard <msuchard@gmail.com>
2015-07-30 15:40:01 -0700

    fix roxygen2 failure

---
+++ R/NPCluster.R
@@ -0,0 +1,11 @@
+# @file NPCluster.R
+#
+# @author Subha Guha
+# @author Marc A. Suchard
+
+#' NPCluster
+#'
+#' @docType package
+#' @name NPCluster
+#' @useDynLib NPCluster, .registration = TRUE
+NULL
