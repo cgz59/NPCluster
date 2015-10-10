4ceba171d0a342b61fb0f009b70be3eec185daac
unknown <guhasu@as-guhasu3.col.missouri.edu>
2015-10-10 11:48:24 -0400

    SG: made changes and then undid them

--- R/gen.X.R
+++ R/gen.X.R
@@ -1,8 +1,14 @@
 
-gen.X <- function(n, p, prop.X.miss, true_parm) {
+gen.X <- function(n, p, prop.X.miss, true_parm, standardize.X=FALSE, flip.sign=FALSE) {
 
 	data <- NULL
 	data$X <- array(,c(n,p))
+
+	data$standardize.X <_ standardize.X
+	data$flip.sign <- flip.sign
+
+	if (data$standardize.X)
+	  {true_parm$location.v <- rnorm(n=p, mean=, sd=6*true_parm$tau)
 
 	for (xx in 1:p)
 	{s.v <- true_parm$clust$s.mt[,true_parm$clust$c.v[xx]]
